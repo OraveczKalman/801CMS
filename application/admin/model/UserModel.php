27ee3a97074b00507efd831c9042cfc4238dfc44
Oravecz Kálmán <orave@DESKTOP-14VH4IN>
2017-04-29 18:26:09 +0200

    Added documentation for UserModel

--- application/admin/model/UserModel.php
+++ application/admin/model/UserModel.php
@@ -2,7 +2,14 @@
 class UserModel {
     private $dataArray;
     private $db;
-
+    
+    /**
+     * 
+     * @param type $db
+     * @param type $dataArray
+     * @author Oravecz Kálmán
+     * Constructor for User handling database object.
+     */
     public function __construct($db, $dataArray=null) {
         if (!is_null($dataArray)) {
             $this->setDataArray($dataArray);
@@ -10,26 +17,51 @@ class UserModel {
         $this->setDb($db);
     }
     
+    /**
+     * 
+     * @param type $db
+     * @author Oravecz Kálmán
+     * Setter function for database connection object
+     */
     public function setDb($db) {
         $this->db = $db;
     }
-
+    
+    /**
+     * 
+     * @param type $dataArray
+     * @author Oravecz Kálmán
+     * Setter function for user data array
+     */
     public function setDataArray($dataArray) {
         $this->dataArray = $dataArray;
     }
 
+    /**
+     * 
+     * @return array enter query
+     * @author Oravecz Kálmán
+     * User query function returns with user data
+     */
     public function getUser() {
-        $enterQueryString = 'SELECT * FROM user WHERE ' . $this->dataArray[0]['where'];
+        $enterQueryString = "SELECT * FROM user WHERE " . $this->dataArray[0]['where'];
         $enterQuery = $this->db->SelectQuery($enterQueryString);
         return $enterQuery;
     }
 
+    /**
+     * 
+     * @return array $getAllUserQuery
+     * @author Oravecz Kálmán
+     * Function for get all users in database
+     */
     public function getAllUser() {
-        $getAllUserQueryString = 'SELECT * FROM user ORDER BY RightId';
-        $getAllUserQuery = $this->db->SelectQuery($getAllUserQueryString);
+        $getAllUserQueryString = "SELECT * FROM user ORDER BY RightId";
+        $getAllUserQuery = $this->db->selectQuery($getAllUserQueryString);
 
         return $getAllUserQuery;
     }
+    
     
     public function getUserCount() {
         $getUserCountByRightQueryString = "SELECT COUNT(UserId) AS userIdCount FROM user WHERE " . $this->dataArray[0]['where'];
@@ -38,6 +70,12 @@ class UserModel {
         return $getUserCountByRightQuery;
     }
 
+    /**
+     * 
+     * @return type
+     * @author Oravecz Kálmán
+     * New user upload
+     */
     public function newUser() {
         if (!isset($_SESSION['admin']['dataArray']['UserId'])) {
             $_SESSION['admin']['dataArray']['UserId'] = 0;
@@ -45,7 +83,7 @@ class UserModel {
         $newUserQueryString = "INSERT INTO user SET " .
             "Name = '" . $this->dataArray['Name'] . "', " .
             "UserName = '" . $this->dataArray['UserName'] . "', " .
-            "Password = '" . $this->dataArray['Password'] . "', " .
+            "Password = SHA2('" . $this->dataArray['Password'] . "', 512), " .
             "Pwdr = '" . $this->dataArray['Pwdr'] . "', " .
             "Email = '" . $this->dataArray['Email'] . "', " .
             "RightId = '" . $this->dataArray['RightId'] . "', " .
@@ -60,13 +98,19 @@ class UserModel {
         return $newUserQuery;
     }
 
+    /**
+     * 
+     * @return type
+     * @author Oravecz Kálmán
+     * Update user data
+     */
     public function updateUser() {
         $updateUserQueryString = "UPDATE user SET " .
-            "Name = " . $this->dataArray['Name'] . ", " .
-            "UserName = " . $this->dataArray['UserName'] . ", " .
-            "Password" . $this->dataArray['Password'] . ", " .
-            "Pwdr = " . $this->dataArray['Pwdr'] . ", " .
-            "Email = " . $this->dataArray['Email'] . ", " .
+            "Name = '" . $this->dataArray['Name'] . "', " .
+            "UserName = '" . $this->dataArray['UserName'] . "', " .
+            "Password = SHA2('" . $this->dataArray['Password'] . "', 512), " .
+            "Pwdr = '" . $this->dataArray['Pwdr'] . "', " .
+            "Email = '" . $this->dataArray['Email'] . "', " .
             "RightId = " . $this->dataArray['RightId'] . ", " .
             "News = " . $this->dataArray['News'] . ", " .
             "Created = NOW(), " .
@@ -75,11 +119,18 @@ class UserModel {
             "ModifiedBy = " . $_SESSION['admin']['userData']['UserId'] . ", " .
             "Active = " . $this->dataArray['active'] . " " .
             "WHERE UserId = " . $this->dataArray['UserId'];
+        print $updateUserQueryString;
         $updateUserQuery = $this->db->updateQuery($updateUserQueryString);
 
         return $updateUserQuery;
     }
 
+    /**
+     * 
+     * @return int
+     * @author Oravecz Kálmán
+     * User deactivate function
+     */
     public function deleteUser() {
         $deleteUserQueryString = "UPDATE user SET " .
             "Modified = NOW(), " .
@@ -96,6 +147,12 @@ class UserModel {
         }
     }
     
+    /**
+     * 
+     * @return type
+     * @author ORavecz Kálmán
+     * Function for get all user rights from table
+     */
     public function getUserRights() {
         $getUserRightsQueryString = "SELECT * FROM user_rights ";
         if (isset($this->dataArray['where'])) {
