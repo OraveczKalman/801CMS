c3f1b7ce5078fab157b5d3f17e8e7dd52da7f996
orave <orave@DESKTOP-5P226T3>
2018-11-25 18:07:18 +0100

    Article pager added

--- application/site/js/scripts.js
+++ application/site/js/scripts.js
@@ -2,3 +2,8 @@
 function NewsPage(Page, Link) {
     $('#szoveges').load(Link, { event:'newsPageSwitch', page:Page }, function(data) {});
 }
+
+function PageSwitcher(Page) {
+    $('.sheet').css("display", "none");
+    $('#sheet' + Page).css("display", "block");
+}
